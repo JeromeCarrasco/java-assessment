b8b40eb919767d06bf09da134d4bbe9be4d01d9d
JeromeCarrasco <jpm.carrasco@gmail.com>
2022-06-06 12:15:57 +0530

    Refactoring Code : created functions

--- src/Calculator/StringCalculator.java
+++ src/Calculator/StringCalculator.java
@@ -8,30 +8,24 @@ public class StringCalculator {
 
         if(numbers.isEmpty())
             return 0;
-        else if (numbers.contains(",")) {
-            String[] numArr = numbers.split(",|\n");
-
-            int[] numArray = Arrays.stream(numArr).mapToInt(Integer::parseInt).toArray();
-
-            return Arrays.stream(numArray).sum();
-        } else {
-            return Integer.parseInt(numbers);
+        else {
+            return Arrays.stream(getConvertedArr(getInputString(numbers))).sum();
         }
 
     }
 
+    private static String[] getInputString(String numbers) {
+        return numbers.split("[,|\n]");
+    }
 
+    private static int[] getConvertedArr(String[] numArr) {
+        return Arrays.stream(numArr).mapToInt(Integer::parseInt).toArray();
+    }
 
     public static void main(String[] args) {
 
-        int result;
         System.out.println("Entered Main !!!");
         System.out.println("This is a Calculator with string Input");
 
-        //StringCalculator calTestObj = new StringCalculator();
-
-        //result = calTestObj.add("1");
-
-        //System.out.println("final result is "+result);
     }
 }
